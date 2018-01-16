f9c60cf45aaab5b49982eac2827a8b484d72746d
David Bowen <dsbmusic328@gmail.com>
2018-01-16 15:15:35 -0500

    Updating current state. Not much has changed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const App = (props) => {
+    return (
+        <h2 className="jumbotron text-center">
+            {props.headerMessage}
+        </h2>
+    );
+};
+
+App.propTypes = {
+    headerMessage: PropTypes.string
+};
+
+App.defaultProps = {
+    headerMessage: 'Hello!!!!!'
+}
 
 ReactDOM.render(
-    React.createElement('h2', null, 'Hello React'),
+    <App headerMessage="This is the props."/>,
     document.getElementById('root')
 );
